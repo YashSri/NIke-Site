26e35f7dbbb65b272ac5618f3144440daa4243da
Yash Srivastava <156013191+YashSri@users.noreply.github.com>
2025-02-02 21:59:46 +0530

    Update Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,13 @@
+import { useState } from "react";
 import { hamburger } from "../assets/icons";
 import { headerLogo } from "../assets/images";
 import { navLinks } from "../constants";
 
 const Nav = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <header className='padding-x py-8 absolute z-10 w-full'>
+    <header className='fixed top-0 left-0 w-full bg-white z-50 padding-x py-4'>
       <nav className='flex justify-between items-center max-container'>
         <a href='/'>
           <img
@@ -15,7 +18,8 @@ const Nav = () => {
             className='m-0 w-[129px] h-[29px]'
           />
         </a>
-        <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
+        
+        <ul className='hidden lg:flex flex-1 justify-center items-center gap-16'>
           {navLinks.map((item) => (
             <li key={item.label}>
               <a
@@ -27,15 +31,33 @@ const Nav = () => {
             </li>
           ))}
         </ul>
-        <div className='flex gap-2 text-lg leading-normal font-medium font-montserrat max-lg:hidden wide:mr-24'>
+        
+        <div className='hidden lg:flex gap-2 text-lg leading-normal font-medium font-montserrat wide:mr-24'>
           <a href='/'>Sign in</a>
           <span>/</span>
           <a href='/'>Explore now</a>
         </div>
-        <div className='hidden max-lg:block'>
-          <img src={hamburger} alt='hamburger icon' width={25} height={25} />
+        
+        {/* Hamburger Menu */}
+        <div className='lg:hidden'>
+          <button onClick={() => setIsOpen(!isOpen)}>
+            <img src={hamburger} alt='hamburger icon' width={25} height={25} />
+          </button>
         </div>
       </nav>
+      
+      {/* Mobile Menu */}
+      {isOpen && (
+        <ul className='absolute top-full left-0 w-full bg-white/50 backdrop-blur-md flex flex-col items-center py-4'>
+          {navLinks.map((item) => (
+            <li key={item.label} className='py-2'>
+              <a href={item.href} className='font-montserrat text-lg text-black'>
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+      )}
     </header>
   );
 };
